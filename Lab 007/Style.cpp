31f26dca898bf23dc2e8d35c514fb8312e4b1646
Saaif <181657317+luffy-2606@users.noreply.github.com>
2025-04-21 22:40:56 +0500

    Add files via upload

---
+++ Lab 007/Style.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring> 
+using namespace std;
+
+class Style {
+	char color[10];
+	bool isFilled;
+public:
+	void SetFilled(bool filled) {
+		isFilled = filled;
+	}
+
+	void SetColor(const char* c) {
+		strcpy_s(color, sizeof(color), c);
+		color[sizeof(color) - 1] = '\0'; // Ensure null-termination
+	}
+
+	Style(const char* c = "", bool filled = false) {
+		strcpy_s(color, sizeof(color), c);
+		color[sizeof(color) - 1] = '\0'; // Ensure null-termination
+		isFilled = filled;
+		cout << "Style() called\n";
+	}
+
+	Style(const Style& obj) {
+		strcpy_s(color, sizeof(color), obj.color);
+		color[sizeof(color) - 1] = '\0'; // Ensure null-termination
+		isFilled = obj.isFilled;
+		cout << "Style() called\n";
+	}
+
+	~Style() {
+		cout << "~Style() called\n";
+	}
+
+	void print() {
+		cout << "Color: " << color << endl;
+		cout << "Filled: " << (isFilled ? "True" : "False") << endl;
+	}
+};
