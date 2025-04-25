51153eaf400bdb765450ea0d293f76694ce586d2
Saaif <181657317+luffy-2606@users.noreply.github.com>
2025-04-25 22:38:34 +0500

    Update Graduate.h

--- Lab 008/Graduate.h
+++ Lab 008/Graduate.h
@@ -1,34 +1,35 @@
-#pragma-once
-#include<iostream>
+#pragma once
+#include <iostream>
+#include "Student.h"
 using namespace std;
 
-class Graduate : public Student{
-	char*  thesis_topic;
+class Graduate : public Student {
+    char* thesis_topic;
 public:
-	Graduate(char* tp, float c, char* f, char* l, int a) : Student(c, f, l, a)
-	{
-		thesis_topic = new char[strlen(tp) + 1];
-		strcpy_s(thesis_topic, strlen(tp), tp);
-		cout << "Graduate() called";
-	}
-	Graduate() : Student()
-	{
-		thesis_topic = 0;
-		cout << "Graduate() called";
-	}
-	~Graduate()
-	{
-		thesis_topic = 0;
-		cout << "~Graduate() called";
-	}
+    Graduate(const char* tp, float c, const char* f, const char* l, int a) : Student(c, f, l, a) {
+        thesis_topic = new char[strlen(tp) + 1];
+        strcpy_s(thesis_topic, strlen(tp) + 1, tp);
+        cout << "Graduate() called" << endl;
+    }
+    Graduate() : Student(), thesis_topic(nullptr) {
+        cout << "Graduate() called" << endl;
+    }
+    ~Graduate() {
+        delete[] thesis_topic;
+        cout << "~Graduate() called" << endl;
+    }
 
-	void setThesis(char* tp)
-	{
-		thesis_topic = new char[strlen(tp) + 1];
-		strcpy_s(thesis_topic, strlen(tp), tp);
-	}
-	char*  getThesis()
-	{
-		return thesis_topic;
-	}
+    void setThesis(const char* tp) {
+        delete[] thesis_topic;
+        thesis_topic = new char[strlen(tp) + 1];
+        strcpy_s(thesis_topic, strlen(tp) + 1, tp);
+    }
+    char* getThesis() { 
+        return thesis_topic; 
+    }
+
+    void printGraduate()
+    {
+		cout << getFirstName() << " " << getLastName() << " is a graduate student, his cgpa is " << getCGPA() << " and his thesis topic is " << thesis_topic << endl;
+    }
 };
