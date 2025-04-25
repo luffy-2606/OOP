9ceaaf0be22a7969dcb39a55e31e8562ce368b0d
Saaif <181657317+luffy-2606@users.noreply.github.com>
2025-04-25 22:37:32 +0500

    Update Faculty.h

--- Lab 008/Faculty.h
+++ Lab 008/Faculty.h
@@ -1,57 +1,51 @@
-#pragma-once
-#include<iostream>
+#pragma once
+#include <iostream>
+#include "Person.h"
 using namespace std;
 
-class Faculty : public Person{
-	int course_count;
-	char* tel;
+class Faculty : public Person {
+    int course_count;
+    char* tel;
 public:
-	Faculty(int cc, char* t, char* f, char* l, int a) : Person(f, l, a)
-	{
-		if (strlen(t) == 3){
-			tel = new char[4];
-			for (int i = 0; i < 3; i++)
-				tel[i] = t[i];
-			tel[3] = 0;
-			course_count = cc;
-		}
-		else{
-			cout << "incorrect telephone extension number" << endl;
-		}
-		cout << "Faculty() called";
-	}
-	Faculty() : Person()
-	{
-		course_count = 0;
-		tel = new char[4];
-		for (int i = 0; i < 3; i++)
-			tel[i] = '0';
-		tel[3] = 0;
-		cout << "Faculty() called";
-	}
-	~Faculty()
-	{
-		delete[] tel;
-		cout << "~Faculty() called";
-	}
+    Faculty(int cc, const char* t, const char* f, const char* l, int a) : Person(f, l, a), course_count(cc) {
+        if (strlen(t) == 3) {
+            tel = new char[4];
+            strcpy_s(tel, 4, t);
+        }
+        else {
+            tel = nullptr;
+            cout << "Incorrect telephone extension number" << endl;
+        }
+        cout << "Faculty() called" << endl;
+    }
+    Faculty() : Person(), course_count(0) {
+        tel = new char[4];
+        strcpy_s(tel, 4, "000");
+        cout << "Faculty() called" << endl;
+    }
+    ~Faculty() {
+        delete[] tel;
+        cout << "~Faculty() called" << endl;
+    }
 
-	void setCourseCount(int cc)
-	{
-		course_count = cc;
-	}
-	void setTelNum(char* t)
-	{
-		for (int i = 0; i < 3; i++)
-			tel[i] = '0';
-		tel[3] = 0;
-	}
+    void setCourseCount(int cc) { 
+        course_count = cc; 
+    }
+    void setTelNum(const char* t) {
+        delete[] tel;
+        tel = new char[4];
+        strcpy_s(tel, 4, t);
+    }
 
-	int getCourseCount()
-	{
-		return course_count;
-	}
-	char* getTelNum()
-	{
-		return tel;
-	}
+    int getCourseCount() { 
+        return course_count; 
+    }
+    char* getTelNum() { 
+        return tel; 
+    }
+
+    void printFaculty()
+    {
+		cout << "Faculty Member name: " << getFirstName() << " " << getLastName() << ", Age: " << age << ", Number of courses: " << course_count << ", Ext. " << tel << endl;
+    }
 };
