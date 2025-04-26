f47a7ae2696b3fc8ee1cda04838efd09a8ac80cc
Saaif <181657317+luffy-2606@users.noreply.github.com>
2025-04-27 01:13:03 +0500

    Update Lab_Inheritance.cpp

--- Lab 008/Lab_Inheritance.cpp
+++ Lab 008/Lab_Inheritance.cpp
@@ -6,16 +6,45 @@
 #include "Undergraduate.h"
 using namespace std;
 
+int main() {
+    Undergraduate UGS1("Ted Thompson", 3.91,"Ted","Thompson", 22);
+    Faculty F1(2, "420", "Richard", "Karp", 45);
+
+    //-------Output:-------
+    /*Person() called
+    Student() called
+    Undergraduate() called
+    Person() called
+    Faculty() called
+    ~Faculty() called
+    ~Person() called
+    ~Undergraduate() called
+    ~Student() called
+    ~Person() called*/
+    //-------------------
 
 
+    //Exercise 6:
+	//1. Because "age" can be accessed directly in the derived classes, without the need for setter/getter methods
+    //2. The error occurs because the First_Name and Last_Name attributes in the Person class are private, and cannot be directly accessed by the derived classes
 
-int main()
-{
-	Undergraduate UGS1("Ted Thompson", 3.91, "Ted", "Thompson", 22);
-	Faculty F1(2, "420", "Richard", "Karp", 45);
 
-	system("pause");
+	UGS1.printInformation();
+	UGS1.printStudent();
+	F1.printFaculty();
+
+    cout << "===========" << endl;
+
+    Undergraduate u("The Event Locator", 3.91, "Ted", "Thompson", 22);
+    Graduate g("Distributed Algorithms", 3.01, "Arnold", "Gates", 25);
+
+    u.printUndergraduate();
+    g.printGraduate();
+
+    u.printStudent();
+
+    //Exercise 9:
+	//Protected: I am unable to use the printStudent() method in the main function because the Undergraduate class can only use the functions of "Student" in its class, not elsewhere. Due to using protected inheritance.
+	//Private: I am unable to use the printStudent() method in the main function because the Undergraduate class can only use the functions of "Student" in its class, not elsewhere. Due to using private inheritance.
+    return 0;
 }
-
-
-
