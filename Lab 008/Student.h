facac5b069672966ad8d432b69e19d67a33f98dc
Saaif <181657317+luffy-2606@users.noreply.github.com>
2025-04-28 00:39:36 +0500

    Update Student.h

--- Lab 008/Student.h
+++ Lab 008/Student.h
@@ -1,31 +1,30 @@
-#pragma-once
-#include<iostream>
+#pragma once
+#include <iostream>
+#include "Person.h"
 using namespace std;
 
-class Student : public Person{
-	float cgpa;
+class Student : public Person {
+    float cgpa;
 public:
-	Student(float c, char* f, char* l, int a) :Person(f, l, a)
-	{
-		cgpa = c;
-		cout << "Student() called";
-	}
-	Student() :Person()
-	{
-		cout << "Student() called";
-		cgpa = 0;
-	}
-	~Student()
-	{
-		cout << "~Student() called";
-	}
+    Student(float c, const char* f, const char* l, int a) : Person(f, l, a), cgpa(c) {
+        cout << "Student() called" << endl;
+    }
+    Student() : Person(), cgpa(0) {
+        cout << "Student() called" << endl;
+    }
+    ~Student() {
+        cout << "~Student() called" << endl;
+    }
 
-	void setCGPA(float c)
-	{
-		cgpa = c;
-	}
-	float getCGPA()
-	{
-		return cgpa;
-	}
+    void setCGPA(float c) { 
+        cgpa = c; 
+    }
+    float getCGPA() { 
+        return cgpa; 
+    }
+    void printStudent() {
+        cout << getFirstName() << " " << getLastName() << " is " << age << " years old, his cgpa is " << cgpa << endl;
+    }
+
+
 };
