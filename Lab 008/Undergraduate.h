179a0a1716a3fa90f4e7f5b96e20254a3ce91524
Saaif <181657317+luffy-2606@users.noreply.github.com>
2025-04-28 00:40:05 +0500

    Update Undergraduate.h

--- Lab 008/Undergraduate.h
+++ Lab 008/Undergraduate.h
@@ -1,34 +1,37 @@
-#pragma-once
-#include<iostream>
+#pragma once
+#include <iostream>
+#include "Student.h"
 using namespace std;
 
-class Undergraduate : public Student{
-	char* fyp_name;
+class Undergraduate : public Student {
+    char* fyp_name;
 public:
-	Undergraduate(char* fn, float c, char* f, char* l, int a) : Student(c, f, l, a)
-	{
-		fyp_name = new char[strlen(fn) + 1];
-		strcpy_s(fyp_name, strlen(fn), fn);
-		cout << "Undergraduate() called";
-	}
-	Undergraduate() : Student()
-	{
-		fyp_name = 0;
-		cout << "Undergraduate() called";
-	}
-	~Undergraduate()
-	{
-		fyp_name = 0;
-		cout << "~Undergraduate() called";
-	}
+    Undergraduate(const char* fn, float c, const char* f, const char* l, int a) : Student(c, f, l, a) {
+        fyp_name = new char[strlen(fn) + 1];
+        strcpy_s(fyp_name, strlen(fn) + 1, fn);
+        cout << "Undergraduate() called" << endl;
+    }
+    Undergraduate() : Student(), fyp_name(nullptr) {
+        cout << "Undergraduate() called" << endl;
+    }
+    ~Undergraduate() {
+        delete[] fyp_name;
+        cout << "~Undergraduate() called" << endl;
+    }
 
-	void setFYP(char* fn)
-	{
-		fyp_name = new char[strlen(fn) + 1];
-		strcpy_s(fyp_name, strlen(fn), fn);
-	}
-	char* getFYP()
-	{
-		return fyp_name;
-	}
+    void setFYP(const char* fn) {
+        delete[] fyp_name;
+        fyp_name = new char[strlen(fn) + 1];
+        strcpy_s(fyp_name, strlen(fn) + 1, fn);
+    }
+    char* getFYP() { 
+        return fyp_name; 
+    }
+
+    void printUndergraduate()
+    {
+        printStudent();
+		cout << getFirstName() << " " << getLastName() << " is an undergraduate student, his cgpa is " << getCGPA() << " and his final year project is titled " << fyp_name << endl;
+    }
+   
 };
