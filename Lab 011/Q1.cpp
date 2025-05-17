2d4be277e006c904153d5522e7ad21b3ee293814
Saaif <181657317+luffy-2606@users.noreply.github.com>
2025-05-17 15:36:29 +0500

    Update Q1.cpp

--- Lab 011/Q1.cpp
+++ Lab 011/Q1.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int a, b;
+    try {
+        cout << "Enter the First Number: ";
+        cin >> a;
+        if (cin.fail()) {
+            throw "Invalid input for the First Number.";
+        }
+
+        cout << "Enter the Second Number: ";
+        cin >> b;
+        if (cin.fail()) {
+            throw "Invalid input for the Second Number.";
+        }
+
+        if (b == 0)
+            throw 0;
+        else
+            cout << "\nDividing the two numbers: " << a / (b * 1.0) << endl;
+    }
+    catch (const char* msg) {
+        cout << "\nError: " << msg << endl;
+    }
+    catch (int e) {
+        cout << "\nError: Division by zero is not allowed." << endl;
+    }
+
+    return 0;
+}
